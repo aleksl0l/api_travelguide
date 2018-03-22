12b5aafcbf95f61014840ce44aa13ba21a966043
aleksl0l <aleksl0l@yandex.ru>
2018-03-22 13:19:26 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,133 @@
+from flask import request, Flask, jsonify, Response
+import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import exc, case
+from sqlalchemy.sql import select
+from model import db, Country, Town, Sights, Base
+
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+
+app = Flask(__name__)
+
+POSTGRES_URL = get_env_variable("POSTGRES_URL")
+POSTGRES_USER = get_env_variable("POSTGRES_USER")
+POSTGRES_PW = get_env_variable("POSTGRES_PW")
+POSTGRES_DB = get_env_variable("POSTGRES_DB")
+
+DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,
+                                                      pw=POSTGRES_PW,
+                                                      url=POSTGRES_URL,
+                                                      db=POSTGRES_DB)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # silence the deprecation warning
+app.config['JSON_AS_ASCII'] = False
+engine = create_engine(DB_URL)
+db.init_app(app)
+
+Session = sessionmaker(engine)
+session = Session()
+Base.metadata.create_all(engine)
+
+
+@app.route('/api_v1.0')
+def api_root():
+    return Response()
+
+
+@app.route('/api_v1.0/create_country')
+def api_create_country():
+    print(request.args)
+    if 'name' in request.args:
+        new_count = Country(name=request.args['name'])
+        try:
+            session.add(new_count)
+            session.commit()
+        except exc.IntegrityError:
+            session.rollback()
+            return jsonify({"success": False})
+    return jsonify({"success": True})
+
+
+@app.route('/api_v1.0/get_countries')
+def api_get_countries():
+    d = {}
+    q = session.query(Country)
+    if 'id_country' in request.args:
+        q = q.filter(Country.id_country == request.args['id_country'])
+    for i, country in enumerate(q):
+        d[i] = {'id_town': country.id_country, 'name': country.name}
+    return jsonify(d)
+
+
+@app.route('/api_v1.0/create_town')
+def api_create_town():
+    print(request.args)
+    if 'name' in request.args:
+        try:
+            new_town = Town(name=request.args['name'], id_country=2)
+            session.add(new_town)
+            session.commit()
+        except exc.IntegrityError:
+            session.rollback()
+            return jsonify({"success": False})
+    return jsonify({"success": True})
+
+
+@app.route('/api_v1.0/get_towns')
+def api_get_towns():
+    d = {}
+    q = session.query(Town)
+    if 'id_country' in request.args:
+        q.filter(Town.id_country == request.args['id_country'])
+    for i, town in enumerate(q):
+        d[i] = {'id_town': town.id_town, 'name': town.name, 'description': town.description}
+    return jsonify(d)
+
+
+@app.route('/api_v1.0/create_sight')
+def api_create_sights():
+    print(request.args)
+    if 'id_town' in request.args and 'name' in request.args:
+        try:
+            new_sight = Sights(id_town=request.args['id_town'], name=request.args['name'])
+            session.add(new_sight)
+            session.commit()
+        except exc.IntegrityError:
+            session.rollback()
+            return jsonify({"success": False})
+        return jsonify({"success": True})
+
+
+@app.route('/api_v1.0/get_sights')
+def api_get_sights():
+    d = {}
+    q = session.query(Sights)
+    if 'id_town' in request.args:
+        q = q.filter(Sights.id_town == request.args['id_town'])
+        for i, sight in enumerate(q):
+            d[i] = {'id_town': sight.id_town,
+                    'name': sight.name,
+                    'tags': sight.tag,
+                    'cost': sight.cost,
+                    'coordinate': sight.coordinate,
+                    'rating': sight.rating,
+                    'type': sight.type_sight
+                    }
+        return jsonify(d)
+    else:
+        return jsonify({"success": False})
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ model.py
@@ -0,0 +1,91 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from sqlalchemy import Column, Integer, String, REAL, Text, ForeignKey, Sequence, ARRAY
+from sqlalchemy.orm import relationship
+from geoalchemy2 import Geometry
+from sqlalchemy.ext.declarative import declarative_base
+import re
+import sqlalchemy.types as types
+
+class Point(types.UserDefinedType):
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def get_col_spec(self, **kw):
+        return 'POINT(%s,%s)' % (self.x, self.y)
+
+    def bind_processor(self, dialect):
+        def process(value):
+            return value
+        return process
+
+    def result_processor(self, dialect, coltype):
+        def process(value):
+            lat = None
+            long = None
+            if value:
+                m = re.match(r"\(([^)]+),([^)]+)\)", value)
+                lat = m.group(1)
+                long = m.group(2)
+            return {'lat': lat, 'long': long}
+        return process
+
+db = SQLAlchemy()
+
+Base = declarative_base()
+
+
+class Country(Base, db.Model):
+    __tablename__ = 'countries'
+    id_country_seq = Sequence('countries_id_country_seq', metadata=Base.metadata)
+
+    id_country = Column(Integer, id_country_seq,
+                        server_default=id_country_seq.next_value(),
+                        primary_key=True)
+    name = Column(String(40), nullable=False)
+
+
+class Town(Base, db.Model):
+    __tablename__ = 'towns'
+
+    id_town_seq = Sequence('towns_id_town_seq', metadata=Base.metadata)
+
+    id_town = Column(Integer,
+                     server_default=id_town_seq.next_value(),
+                     primary_key=True)
+
+    name = Column(String(40), nullable=False)
+    description = Column(Text)
+    id_country = Column(Integer, ForeignKey('countries.id_country'))
+
+    id_country_rel = relationship('Country', foreign_keys=[id_country])
+
+
+class Sights(Base, db.Model):
+    __tablename__ = 'sights'
+
+    id_sights_seq = Sequence('sights_id_sights_seq', metadata=Base.metadata)
+
+    id_sights = Column(Integer,
+                       server_default=id_sights_seq.next_value(),
+                       primary_key=True)
+    name = Column(String(100), nullable=False)
+    tag = Column(ARRAY(Text))
+    cost = Column(REAL)
+    id_town = Column(Integer, ForeignKey('towns.id_town'))
+    coordinate = Column(Point)
+    rating = Column(REAL)
+    type_sight = Column(Text)
+
+
+    id_town_rel = relationship('Town', foreign_keys=[id_town])
+
+# class Setsandwords(Base, db.Model):
+#     __tablename__ = 'setsandwords'
+#
+#     set_id = Column(Integer, ForeignKey('sets.set_id'), primary_key=True)
+#     word_id = Column(Integer, ForeignKey('words.word_id'), primary_key=True)
+#
+#     set_id_rel = relationship('Sets', foreign_keys=[set_id])
+#     word_id_rel = relationship('Words', foreign_keys=[word_id])
