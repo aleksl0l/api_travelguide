11714f0add067c9a5651cb8cabeac0bdfd606e74
aleksl0l <aleksl0l@yandex.ru>
2018-09-02 21:02:05 +0300

    add codes in likes

--- app/likes/views.py
+++ app/likes/views.py
@@ -7,7 +7,7 @@ from sqlalchemy import exc
 likes = Blueprint('likes', __name__)
 
 
-@likes.route('/api_v1.0/add_like', methods=['GET', 'POST'])
+@likes.route('/api_v1.0/add_like', methods=['POST'])
 @token_required
 def api_add_like(current_user):
     try:
@@ -15,16 +15,16 @@ def api_add_like(current_user):
         new_like = Likes(id_user=current_user.id_user, id_sight=id_sight, value=1)
         session.add(new_like)
         session.commit()
-        return jsonify({'message': None, 'data': None, 'status': 'success'})
+        return jsonify({'message': None, 'data': None, 'status': 'success'}), 200
     except exc.IntegrityError:
         session.rollback()
-        return jsonify({'message': 'Duplicate', 'data': None, 'status': 'error'})
+        return jsonify({'message': 'Duplicate', 'data': None, 'status': 'error'}), 400
     except Exception as e:
         session.rollback()
-        return jsonify({'message': 'Unexpected error', 'data': None, 'status': 'error'})
+        return jsonify({'message': 'Unexpected error', 'data': None, 'status': 'error'}), 400
 
 
-@likes.route('/api_v1.0/del_like', methods=['GET', 'POST'])
+@likes.route('/api_v1.0/del_like', methods=['POST'])
 @token_required
 def api_del_like(current_user):
     try:
@@ -35,7 +35,7 @@ def api_del_like(current_user):
                                             ).first()
         session.delete(del_like)
         session.commit()
-        return jsonify({'message': None, 'data': None, 'status': 'success'})
+        return jsonify({'message': None, 'data': None, 'status': 'success'}), 200
     except Exception as e:
         session.rollback()
-        return jsonify({'message': 'Unexpected error', 'data': None, 'status': 'error'})
+        return jsonify({'message': 'Unexpected error', 'data': None, 'status': 'error'}), 400
--- app/sights/models.py
+++ app/sights/models.py
@@ -2,30 +2,6 @@ from sqlalchemy import Column, Integer, String, REAL, Text, ForeignKey, ARRAY
 # from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-#
-# class Point(types.UserDefinedType):
-#     def __init__(self, x=0, y=0):
-#         self.x = x
-#         self.y = y
-#
-#     def get_col_spec(self, **kw):
-#         return '(%s,%s)' % (self.x, self.y)
-#
-#     def bind_processor(self, dialect):
-#         def process(value):
-#             return value
-#         return process
-#
-#     def result_processor(self, dialect, coltype):
-#         def process(value):
-#             lat = None
-#             long = None
-#             if value:
-#                 m = re.match(r"\(([^)]+),([^)]+)\)", value)
-#                 lat = m.group(1)
-#                 long = m.group(2)
-#             return {'lat': lat, 'long': long}
-#         return process
 from app.towns.models import Town
 
 # from app.app import db
